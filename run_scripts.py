4152cdef839ef58b08bc6e89e39de4977f7795e8
Jeff-Kwan <informaverse@gmail.com>
2026-02-23 15:20:19 +0000

    hyperparameter search

--- run_scripts.py
+++ run_scripts.py
@@ -11,3 +11,15 @@ subprocess.run(
     cwd="/workspace/miccai2026",
     check=True
 )
+
+subprocess.run(
+    ["torchrun", "--nproc_per_node=4", "EchoDynaDDP2.py"],
+    cwd="/workspace/miccai2026",
+    check=True
+)
+
+subprocess.run(
+    ["torchrun", "--nproc_per_node=4", "EchoDynaDDP3.py"],
+    cwd="/workspace/miccai2026",
+    check=True
+)
